618a18a025aef0db15b7b772e71a87ca81fe9d15
pss940909 <pss940909@gmail.com>
2023-07-07 17:24:58 +0800

    upload dist

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,17 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <base-card></base-card>
 </template>
+
+<script>
+import BaseCard from "../components/BaseCard.vue";
+export default {
+  components: {
+    BaseCard,
+  },
+  data() {},
+  methods: {},
+  mounted() {},
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/vuecli3/dist/",
 });
